55089ec6ac0133f3b55b361c353233c7948c925d
Bert Spaan <bert.spaan@gmail.com>
2022-07-11 14:51:52 +0200

    new sveltekit version, new annotation urls

--- src/lib/WarpedMapWebGLRenderer.ts
+++ src/lib/WarpedMapWebGLRenderer.ts
@@ -191,7 +191,7 @@ export class WarpedMapWebGLRenderer extends BaseObject {
     gl.bindTexture(gl.TEXTURE_2D, this.tilesTexture)
 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 
